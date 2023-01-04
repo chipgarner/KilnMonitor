3c2a8e87c3ead1a5dfcbff6a930c485a81b90ebe
James Kirikland Garner <chip@garnertotalenergy.com>
2023-01-04 15:40:50 -0800

    Two themocouples on one SPI

---
+++ KilnTemps.py
@@ -0,0 +1,32 @@
+import time
+import board
+import busio
+import digitalio
+import adafruit_max31865
+import adafruit_max31855
+
+print('MAX31865 and MAX31855 two thermocouples')
+
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+cs1 = digitalio.DigitalInOut(board.D5)
+cs2 = digitalio.DigitalInOut(board.D18)
+
+sensor1 = adafruit_max31865.MAX31865(spi, cs1)
+sensor2 = adafruit_max31855.MAX31855(spi, cs2)
+
+
+while True:
+	temp1 = sensor1.temperature
+	temp2 = sensor2.temperature
+
+	for k, v in sensor1.fault.items():
+		if v:
+			print('Sensor1 fault: ' + str(v))
+	for k, v in sensor2.fault.items():
+		if v:
+			print('Sensor2 fault: ' + str(v))
+
+	print('Temperature: {0:0.3f}C'.format(temp1))
+	print('Temperature: {0:0.3f}C'.format(temp2))
+
+	time.sleep(0.1)
