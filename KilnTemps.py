6cb89e4cc6f5ae4290fee69580079a93341bd902
James Kirikland Garner <chip@garnertotalenergy.com>
2023-01-04 15:54:07 -0800

    00ps

--- KilnTemps.py
+++ KilnTemps.py
@@ -2,7 +2,7 @@ import time
 import board
 import busio
 import digitalio
-import adafruit_max31865
+import adafruit_max31856
 import adafruit_max31855
 
 print('MAX31865 and MAX31855 two thermocouples')
@@ -11,7 +11,7 @@ spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 cs1 = digitalio.DigitalInOut(board.D5)
 cs2 = digitalio.DigitalInOut(board.D18)
 
-sensor1 = adafruit_max31865.MAX31865(spi, cs1)
+sensor1 = adafruit_max31856.MAX31856(spi, cs1)
 sensor2 = adafruit_max31855.MAX31855(spi, cs2)
 
 
