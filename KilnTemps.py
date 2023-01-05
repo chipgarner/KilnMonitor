d8f26d38719f7f4b56eaaa41e82e8c12f44533b4
James Kirikland Garner <chip@garnertotalenergy.com>
2023-01-04 16:24:38 -0800

    Try catch for 55 errors

--- KilnTemps.py
+++ KilnTemps.py
@@ -17,16 +17,17 @@ sensor2 = adafruit_max31855.MAX31855(spi, cs2)
 
 while True:
 	temp1 = sensor1.temperature
-	temp2 = sensor2.temperature
+
+	try:
+		temp2 = sensor2.temperature
+	except RuntimeError as ex:
+		print('31855 crash: ' + str(ex))
 
 	for k, v in sensor1.fault.items():
 		if v:
 			print('Sensor1 fault: ' + str(v))
-	# for k, v in sensor2.fault.items():
-	# 	if v:
-	# 		print('Sensor2 fault: ' + str(v))
 
-	print('Temperature: {0:0.3f}C'.format(temp1))
-	print('Temperature: {0:0.3f}C'.format(temp2))
+	print('Temperature1: {0:0.3f}C'.format(temp1))
+	print('Temperature2: {0:0.3f}C'.format(temp2))
 
 	time.sleep(0.1)
