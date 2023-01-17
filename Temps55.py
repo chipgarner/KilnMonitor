84aa8c6f3e3a46fbc543fc4fe2fb73bf39c1e977
James Kirikland Garner <chip@garnertotalenergy.com>
2023-01-17 11:10:04 -0800

    Separate 55 and 56 modules

--- Temps55.py
+++ Temps55.py
@@ -5,19 +5,15 @@ import logging
 import board
 import busio
 import digitalio
-import adafruit_max31856
 import adafruit_max31855
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(name)s: %(message)s')
-logging.info('Get the temperatures, MAX31865 and MAX31855 two thermocouples')
+logging.info('Get the temperatures, MAX31855')
 
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-cs1 = digitalio.DigitalInOut(board.D5)
 cs2 = digitalio.DigitalInOut(board.D6)
 
-sensor1 = adafruit_max31856.MAX31856(spi, cs1)
 sensor2 = adafruit_max31855.MAX31855(spi, cs2)
-sensor1.averaging = 16
 
 
 def c_to_f(c):
@@ -31,8 +27,8 @@ if publish_me:
     pub = Publish.publisher.Publisher(TEST_SECRET)
 
 
-    def publish_results(temp, t2):
-        message = {'T1 56': c_to_f(temp), 'T2 55': c_to_f(t2)}
+    def publish_results(t2):
+        message = {'T2 55': c_to_f(t2)}
         time_in_seconds = round(time.time() * 1000)
         time_stamped_message = {"ts": time_in_seconds, "values": message}
         pub.send_message(str(time_stamped_message))
@@ -45,9 +41,6 @@ last_t2 = 0  # Save this and re-use on errors
 t2_cold_junction = None
 
 while True:
-    temp1 = sensor1.temperature
-    temp1_cj = sensor1.reference_temperature
-
     try:
         temp2 = sensor2.temperature
         last_t2 = temp2
@@ -56,16 +49,10 @@ while True:
         logging.error('Temp2 31855 crash: ' + str(ex))
         temp2 = last_t2
 
-    for k, v in sensor1.fault.items():
-        if v:
-            logging.error('Temp1 31856 fault: ' + str(k))
-
-    logging.info('T1 56: {0:0.3f}F'.format(c_to_f(temp1)))
-    logging.info('T1 cold junction: {0:0.3f}F'.format(c_to_f(temp1_cj)))
     logging.info('T2 55: {0:0.3f}F'.format(c_to_f(temp2)))
     logging.info('T2 cold junction: {0:0.3f}F'.format(c_to_f(t2_cold_junction)))
     logging.info('  ')
 
-    publish_results(temp1, temp2)
+    publish_results(temp2)
 
-    time.sleep(5)
+    time.sleep(1)
--- Temps56.py
+++ Temps56.py
@@ -6,17 +6,14 @@ import board
 import busio
 import digitalio
 import adafruit_max31856
-import adafruit_max31855
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(name)s: %(message)s')
-logging.info('Get the temperatures, MAX31865 and MAX31855 two thermocouples')
+logging.info('MAX31865 temperatures')
 
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 cs1 = digitalio.DigitalInOut(board.D5)
-cs2 = digitalio.DigitalInOut(board.D6)
 
 sensor1 = adafruit_max31856.MAX31856(spi, cs1)
-sensor2 = adafruit_max31855.MAX31855(spi, cs2)
 sensor1.averaging = 16
 
 
@@ -31,30 +28,19 @@ if publish_me:
     pub = Publish.publisher.Publisher(TEST_SECRET)
 
 
-    def publish_results(temp, t2):
-        message = {'T1 56': c_to_f(temp), 'T2 55': c_to_f(t2)}
+    def publish_results(temp):
+        message = {'T1 56': c_to_f(temp)}
         time_in_seconds = round(time.time() * 1000)
         time_stamped_message = {"ts": time_in_seconds, "values": message}
         pub.send_message(str(time_stamped_message))
 else:
-    def publish_results(temp, t2):
+    def publish_results(temp):
         pass
 
-
-last_t2 = 0  # Save this and re-use on errors
-t2_cold_junction = None
 
 while True:
     temp1 = sensor1.temperature
     temp1_cj = sensor1.reference_temperature
-
-    try:
-        temp2 = sensor2.temperature
-        last_t2 = temp2
-        t2_cold_junction = sensor2.reference_temperature
-    except RuntimeError as ex:
-        logging.error('Temp2 31855 crash: ' + str(ex))
-        temp2 = last_t2
 
     for k, v in sensor1.fault.items():
         if v:
@@ -62,10 +48,8 @@ while True:
 
     logging.info('T1 56: {0:0.3f}F'.format(c_to_f(temp1)))
     logging.info('T1 cold junction: {0:0.3f}F'.format(c_to_f(temp1_cj)))
-    logging.info('T2 55: {0:0.3f}F'.format(c_to_f(temp2)))
-    logging.info('T2 cold junction: {0:0.3f}F'.format(c_to_f(t2_cold_junction)))
     logging.info('  ')
 
-    publish_results(temp1, temp2)
+    publish_results(temp1)
 
     time.sleep(5)
