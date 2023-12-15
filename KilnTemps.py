a898104eb60f76b8f0e57d6e49f3fead2d53b592
jameskirklandgarner <chip@garnertotalenergy.com>
2023-12-14 17:54:16 -0800

    Adding 96

--- KilnTemps.py
+++ KilnTemps.py
@@ -4,12 +4,15 @@ from Secrets import TEST_SECRET
 import logging
 import MAX31855
 import MAX31856
+import MCP9600
 
-logging.basicConfig(level=logging.WARNING, format='%(asctime)s %(levelname)s %(name)s: %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(name)s: %(message)s')
 logging.info('Get the temperatures, MAX31865 and MAX31855 two thermocouples')
 
 sensor1 = MAX31856.MAX31856()
 sensor2 = MAX31855.MAX31855()
+sensor3 = MCP9600.MCP9600()
+
 
 publish_me = True
 
@@ -33,10 +36,13 @@ t2_cold_junction = None
 while True:
     temp1 = sensor1.get_temperature()
     temp2 = sensor2.get_temperature()
+    temp3 = sensor3.get_temperature()
+    
 
 
     logging.info('T1 56: {0:0.3f}F'.format(temp1))
     logging.info('T2 55: {0:0.3f}F'.format(temp2))
+    logging.info('9600: {0:0.3f}F'.format(temp3))
     logging.info('  ')
 
     publish_results(temp1, temp2)
