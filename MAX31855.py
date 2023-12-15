91f3f0b41a493726f752439cf534bb8bb6e25eb3
jameskirklandgarner <chip@garnertotalenergy.com>
2023-12-15 15:15:41 -0800

    KilnTemps class fix

--- MAX31855.py
+++ MAX31855.py
@@ -6,7 +6,7 @@ import logging
 
 
 class MAX31855:
-    def __init__(self):
+    def __init__(self, use_nist=False):
         self.logger = logging.getLogger(__name__)
 
         spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
@@ -15,9 +15,14 @@ class MAX31855:
         self.last_t = 0
         self.sensor = adafruit_max31855.MAX31855(spi, cs)
 
+        self.use_nist = use_nist
+
     def get_temperature(self):
         try:
-            temp = self.sensor.temperature
+            if self.use_nist:
+                temp = self.sensor.temperature_NIST
+            else:
+                temp = self.sensor.temperature
             self.last_t = temp
         except RuntimeError as ex:
             self.logger.error('Temp2 31855 crash: ' + str(ex))
